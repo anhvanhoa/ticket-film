fe279d0c6901214074a5a2b750b4422cb182a080
anhvanhoa <anhhoccode@gmail.com>
2024-03-03 07:00:51 +0700

    done header Hoàn thành header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang='en' className='dark'>
+        <html lang='en'>
             <body className={inter.className}>
                 <div className='max-h-screen overflow-hidden'>{children}</div>
             </body>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,21 +3,43 @@ import { Button } from './ui/button';
 import Logo from './logo';
 import Link from 'next/link';
 interface Props {}
+const dataNavigate = [
+    {
+        id: 1,
+        name: 'Mua vé',
+        href: '/'
+    },
+    {
+        id: 2,
+        name: 'Lịch chiếu',
+        href: '/'
+    }
+];
 const header = ({}: Props) => {
     return (
-        <section className='backdrop-blur-sm border-b'>
+        <section className='backdrop-blur-md border-b sticky top-0 z-50 bg-white/70'>
             <div className='max-w-screen-xl mx-auto py-4 flex justify-between items-center'>
                 <div>
-                    <Logo />
+                    <Link href='/'>
+                        <Logo />
+                    </Link>
                 </div>
                 <div className='flex items-center'>
-                    <div className='flex items-center gap-8 *:font-semibold hover:*:text-rose-500 *:transition-all'>
-                        <Link href='/'>Mua vé</Link>
-                        <Link href='/'>Lịch chiếu</Link>
+                    <div className='flex items-center gap-8 *:font-semibold hover:*:text-[#055398] *:transition-all'>
+                        {dataNavigate.map((nav) => (
+                            <Link href={nav.href} key={nav.id}>
+                                {nav.name}
+                            </Link>
+                        ))}
                     </div>
-                    <Button className='border-gray-50/2 ml-10' size={'sm'}>
-                        Đăng nhập
-                    </Button>
+                    <Link href='/login'>
+                        <Button
+                            className='border-gray-50/2 ml-10 bg-[#055398] hover:bg-[#055398]/90 font-semibold'
+                            size='sm'
+                        >
+                            Đăng nhập
+                        </Button>
+                    </Link>
                 </div>
             </div>
         </section>
